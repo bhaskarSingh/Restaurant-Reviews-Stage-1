ccdf1c796d01b055441018770b9047f7cee35ce3
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-09-18 19:03:21 +0530

    feat: use h2 for title to show correct semantics

--- js/main.js
+++ js/main.js
@@ -184,7 +184,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = DBHelper.getImageName(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
